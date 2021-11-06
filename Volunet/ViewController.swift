afb57b519202475f86388be3a964f23f0b846303
sktimokhina <sktimokhina@yandex-team.ru>
2021-11-06 22:45:20 +0300

    Inited the project
    Removed a storyboard

---
+++ Volunet/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Volunet
+//
+//  Created by Софья Тимохина on 06.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
