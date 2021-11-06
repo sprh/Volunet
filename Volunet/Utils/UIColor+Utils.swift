e90523488f95589228647af670ae711874849f21
sktimokhina <sktimokhina@yandex-team.ru>
2021-11-06 23:17:26 +0300

    Added colors, updated ui color extension

--- Volunet/Utils/UIColor+Utils.swift
+++ Volunet/Utils/UIColor+Utils.swift
@@ -6,7 +6,29 @@
 //
 
 import Foundation
+import UIKit
 
+/**
+ UIColor extension with custom colors from assets.
+ */
 extension UIColor {
-    
+    public var background: UIColor {
+        UIColor(named: "backgroundColor")!
+    }
+
+    public var darkTeal: UIColor {
+        UIColor(named: "darkTealColor")!
+    }
+
+    public var lightGreen: UIColor {
+        UIColor(named: "lightGreenColor")!
+    }
+
+    public var lightTeal: UIColor {
+        UIColor(named: "lightTealColor")!
+    }
+
+    public var teal: UIColor {
+        UIColor(named: "tealColor")!
+    }
 }
